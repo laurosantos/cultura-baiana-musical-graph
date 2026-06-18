ae887e50041fb836732b6e048ef4e9eee99c4a8e
Lauro Santos <laurosantos@gmail.com>
2026-06-18 19:37:41 -0300

    Adicionando licença

--- cultura-baiana-musical-graph.php
+++ cultura-baiana-musical-graph.php
@@ -1,10 +1,31 @@
 <?php
 /**
- * Plugin Name: Cultura Baiana Musical Graph
- * Description: Gera grafos JSON-LD musicais personalizados para páginas biográficas e obras musicais.
- * Version: 0.1.0
- * Author: Lauro Santos
+ * Plugin Name:       Cultura Baiana Musical Graph
+ * Description:       Inserção manual de JSON-LD (dados estruturados) em posts, páginas e custom post types.
+ * Version:           0.1.0
+ * Author:            Lauro Santos
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       cultura-baiana-musical-graph
  */
+
+/*
+Cultura Baiana Musical Graph
+Copyright (C) 2026  Lauro Santos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
